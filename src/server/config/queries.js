1b886527164a4e91c133b60fb065d30529ca7734
crocodeev <svin862003@mail.ru>
2020-09-20 13:06:32 +0300

    add get channels api

--- src/server/config/queries.js
+++ src/server/config/queries.js
@@ -3,6 +3,27 @@ module.exports = {
     return "SELECT * FROM campaign WHERE project_id=" + id + ";"
   },
   channels: function (id) {
-    return "SELECT * FROM campaign_channel WHERE campaign_id=" + id + ";"
+    return "SELECT channel.name FROM channel INNER JOIN campaign_channel ON channel.channel_id=campaign_channel.channel_id INNER JOIN campaign ON campaign_channel.campaign_id=campaign.campaign_id WHERE campaign.campaign_id=" + id + ";"
   }
 }
+
+
+/*
+
+плейлист
+
+SELECT * FROM `playlist` WHERE campaign_id=207
+
+медиа
+
+SELECT * FROM `playlist_media` WHERE playlist_id=411
+
+далее
+
+SELECT * FROM `media_domain` WHERE media_domain_id=340627
+
+SELECT * FROM `media` WHERE media_id=388188
+
+общий
+
+*/
